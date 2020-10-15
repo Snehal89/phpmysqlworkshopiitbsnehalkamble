3aa3acaf346057dad52ba68b2f7286adc167b2b8
Snehal89 <kamblesnepa18ce@student.mes.ac.in>
2020-10-15 21:35:13 +0530

    day 6,7 uploaded

--- Day 5/connect1.php
+++ Day 5/connect1.php
@@ -4,6 +4,6 @@
 $conn = mysqli_connect("localhost","root","","data1") or die(mysqli_connect_error());
 //mysqli_select_db($conn,"data1") or die(mysqli_error($conn));
 
-echo "Connected! <br/>"
+echo "Connected! <br/>";
 
 ?>
